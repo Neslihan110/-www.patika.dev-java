66cc120327d8bc7ce4eb19e543bac6fd1a674f78
Neslihan110 <116304785+Neslihan110@users.noreply.github.com>
2022-10-22 13:44:42 +0300

    Add files via upload

---
+++ vucutKitleIndeki/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double boy, m;
+        int kilo, kg;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("lütfen boyunuzu metre cinsinden giriniz  : ");
+        boy =input.nextDouble();
+
+        System.out.println("lütfen kilonuzu  giriniz  : ");
+        kilo=input.nextInt();
+
+        double indeks= (kilo / boy * boy);
+        System.out.println("vücut kitle indeksiniz : " + indeks);
+
+
+
+    }
+}
