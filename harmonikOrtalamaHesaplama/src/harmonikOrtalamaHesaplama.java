61bc0ea8a7a5f28c119a2953193f6104d15114a2
Neslihan <116304785+Neslihan110@users.noreply.github.com>
2022-11-04 11:10:27 +0300

    Add files via upload

---
+++ harmonikOrtalamaHesaplama/src/harmonikOrtalamaHesaplama.java
@@ -0,0 +1,15 @@
+public class harmonikOrtalamaHesaplama {
+
+    public static void main(String[] args) {
+
+      int [] array ={1,2,3,4,5};
+      double total;
+      float result =0;
+      for (int j : array) {
+          result += 1f / j;
+      }
+      total=array.length / result;
+
+        System.out.println("harmonic Average = " + total);
+    }
+}
