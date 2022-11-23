c24e4769d1878451d6e50c60de1e013a8eddbd02
Neslihan <116304785+Neslihan110@users.noreply.github.com>
2022-11-23 17:40:12 +0300

    Add files via upload

---
+++ patikaStore/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+                patikaStore patikaStore = new patikaStore();
+                patikaStore.run();
+            }
+        }
